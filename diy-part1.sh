51c1b8eadbc1bf2ed97ec2151cbb3689e39a6765
PLUTO-JY <95289833+PLUTO-JY@users.noreply.github.com>
2021-12-04 19:43:35 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -3,7 +3,7 @@
 
 # Add a feed source
 # echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default                                     #ssr-plus
-# echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default                             #passwall
+echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default                             #passwall
 # echo 'src-git liuran001_packages https://github.com/liuran001/openwrt-packages' >>feeds.conf.default                   #liuran001软件库
 # echo 'src-git small8 https://github.com/kenzok8/small-package' >>feeds.conf.default                                    #kenzok8    
 echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default                                  #kenzok8软件库
